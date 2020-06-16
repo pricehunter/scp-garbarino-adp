972fce333a8c416deb1cc6f750dd7985cea7b5f8
Federico Marilungo <federico.marilungo@redb.ee>
2020-06-16 10:18:25 -0300

    first scrapper script example

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # scp-garbarino-adp
 Scrapper for Garbarino Site
+
+# Made Following this tutorial
+https://realpython.com/beautiful-soup-web-scraper-python/
--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.monster.com/jobs/search/?q=Software-Developer&where=Australia'
+page = requests.get(URL)
+
+print(page.content)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+#First Find element
+results = soup.find(id='ResultsContainer')
+print(results.prettify())
+
 print("Goodbye, World!")
